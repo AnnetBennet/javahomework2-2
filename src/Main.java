7dce2720025763128800d0d3cb0e2950690d94b8
Lumos maxima <eanechka91@gmail.com>
2024-04-12 02:09:22 +0500

    Change src

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int wallet = 300;
+        int summa = 1100;
+        int bonus;
+        if (summa < 1000) {
+            bonus = 0;
+        } else {
+            bonus = summa / 100;
+        }
+        System.out.println(bonus);
     }
-}
+    }
