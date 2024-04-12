ffabcc7ea593c03cbaad2d4cc2514198e2dba9d9
Lumos maxima <eanechka91@gmail.com>
2024-04-12 14:56:11 +0500

    Chanched src

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         int wallet = 300;
-        int summa = 1100;
+        int amount = 1100;
         int bonus;
-        if (summa < 1000) {
+        if (amount < 1000) {
             bonus = 0;
         } else {
-            bonus = summa / 100;
+            bonus = amount / 100;
         }
         System.out.println(bonus);
     }
-    }
+}
